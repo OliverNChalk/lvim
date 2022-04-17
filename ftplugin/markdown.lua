3bce0a726dcd02dd2bc257559acfe4b6d06c1600
Oliver Chalk <oliverchalk245@gmail.com>
2022-04-17 16:35:35 +1000

    markdown: 80 char column width

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -8,4 +8,5 @@ formatters.setup {
 
 vim.cmd[[setlocal shiftwidth=2]]
 vim.cmd[[setlocal tabstop=2]]
+vim.cmd[[setlocal textwidth=80]]
 -- vim.list_extend(lvim.lsp.override, { "ltex", "tailwindcss" })
